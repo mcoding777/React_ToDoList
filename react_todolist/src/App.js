abb2770ec378ff8d893c901a5e53714556615c03
misun <eating8@naver.com>
2021-11-29 21:23:03 +0900

    input과 button 구현

--- react_todolist/src/App.js
+++ react_todolist/src/App.js
@@ -1,7 +1,25 @@
+import { useImperativeHandle, useState } from "react";
+
 function App() {
+
+  const [todo, setTodo] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(todo)
+  }
+
   return (
-    <div className="App">
-      <h1>Hello, React</h1>
+    <div>
+      <h1>오늘의 할일</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          value={todo}
+          type="text"
+          placeholder="해야할 일을 여기에 적어주세요"
+          onChange={(event) => {setTodo(event.target.value);}} />
+        <button type="submit">저장</button>
+      </form>
     </div>
   );
 }
