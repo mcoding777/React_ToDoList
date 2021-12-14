af75d3ef41540504ae490f02a20206aa843289a2
misun <mcoding777@naver.com>
2021-12-14 20:50:08 +0900

    CSS 추가 중

--- react_todolist/src/App.js
+++ react_todolist/src/App.js
@@ -32,28 +32,34 @@ function App() {
   }
 
   return (
-    <div>
-      <h1>오늘의 할일 ({todos.length}개)</h1>
-      <form action="" onSubmit={handleSubmit}>
-        <input
-          value={todo}
-          type="text"
-          placeholder="해야할 일을 여기에 적어주세요"
-          onChange={(event) => {setTodo(event.target.value);}} />
-        <button type="submit">저장</button>
-      </form>
-      <hr />
-      <ul>
-        {todos.map((item, index) => (
-          <li>{item.value}
-            <button type="button" 
-              onClick={() => handleComplete(index)}>완료</button>
-            <button type="button">수정</button>
-            <button type="button">삭제</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <header>
+        <h1>오늘의 할일 ({todos.length}개)</h1>
+        <div className="container">
+          <form action="" onSubmit={handleSubmit} className="inputform">
+            <input
+              value={todo}
+              type="text"
+              placeholder="해야할 일을 여기에 적어주세요"
+              onChange={(event) => {setTodo(event.target.value);}} />
+            <button type="submit">저장</button>
+          </form>
+        </div>
+        <hr />
+      </header>
+      <main>
+        <ul>
+          {todos.map((item, index) => (
+            <li>{item.value}
+              <button type="button" 
+                onClick={() => handleComplete(index)}>완료</button>
+              <button type="button">수정</button>
+              <button type="button">삭제</button>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
   );
 }
 
